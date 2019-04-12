de553a3d688d21ea88a83ab615e0637c7f365321
marisa10brink <m10brink@gmail.com>
2019-04-12 15:45:39 -0500

    trying to get carousel working

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,1 +1,11 @@
 // JavaScript Document
+
+$(document).ready(function(){
+  $('.jumbotron').slick({
+    autoplay: true,
+    autoplaySpeed: 4000, 
+    arrows: false,
+    draggable: false,
+    
+  });
+});
